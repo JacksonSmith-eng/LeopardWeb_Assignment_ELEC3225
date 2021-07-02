9579986f3991bd70c59ac3d001746741c8d89162
JacksonSmith-eng <72995302+JacksonSmith-eng@users.noreply.github.com>
2021-07-02 00:02:20 -0400

    Update userHome.cpp

--- userHome.cpp
+++ userHome.cpp
@@ -3,7 +3,7 @@
 /////////////////////////////////////////////////////
 // * * * * * * * * * * STUDENT * * * * * * * * * * //
 /////////////////////////////////////////////////////
-void studentHome(student_c* student) {
+void studentHome(const char* dir, student_c* student) {
 
 	int signedIn = 1;
 
@@ -14,22 +14,21 @@ void studentHome(student_c* student) {
 		std::cout << "1: Search courses\n";
 		std::cout << "2: Add/Drop courses\n";
 		std::cout << "3: Display schedule\n";
-		std::cout << "4: Display GPA\n";
-		std::cout << "5: Display all student info\n";
+		std::cout << "4: Display all student info\n";
 		std::cout << "0: Sign out\n";
 		std::cin >> choice;
+		//choice = 1; // * * * TEST * * *
 		std::cout << std::endl;
 
 		switch (choice) {
-			case 1: { student->searchCourses(); break; }
-			case 2: { student->addDropCourses(); break; }
-			case 3: { student->printSchedule(); break; }
-			case 4: { student->printGPA(); break; }
-			case 5: { student->printAll(); break; }
+			case 1: { student->searchCourses(dir); break; }
+			case 2: { student->addDropCourse(dir); break; }
+			case 3: { student->printSchedule(dir); break; }
+			case 4: { student->printAll(); break; }
 			case 0: {
 				do {
 					if (signedIn != 0 && signedIn != 1)
-						std::cout << "Please enter a valid input.\n\n";
+						std::cout << "Please enter a valid choice.\n\n";
 					std::cout << "You wish to sign out?\n";
 					std::cout << "1: Stay signed in\n";
 					std::cout << "0: Sign out\n";
@@ -38,7 +37,7 @@ void studentHome(student_c* student) {
 				} while (signedIn != 0 && signedIn != 1);
 				break;
 			}
-			default: { std::cout << "Please enter a valid input.\n\n"; }
+			default: { std::cout << "Please enter a valid choice.\n\n"; }
 		}
 	}
 }
@@ -48,7 +47,7 @@ void studentHome(student_c* student) {
 ////////////////////////////////////////////////////////
 // * * * * * * * * * * INSTRUCTOR * * * * * * * * * * //
 ////////////////////////////////////////////////////////
-void instructorHome(instructor_c* instructor) {
+void instructorHome(const char* dir, instructor_c* instructor) {
 
 	int signedIn = 1;
 
@@ -57,22 +56,20 @@ void instructorHome(instructor_c* instructor) {
 
 		std::cout << " * * * Instructor Home * * *\n";
 		std::cout << "1: Search courses\n";
-		std::cout << "2: Display schedule\n";
-		std::cout << "3: Display classlist\n";
-		std::cout << "4: Display all instructor info\n";
+		std::cout << "2: Display Course Roster\n";
+		std::cout << "3: Display all instructor info\n";
 		std::cout << "0: Sign out\n";
 		std::cin >> choice;
 		std::cout << std::endl;
 
 		switch (choice) {
 			case 1: { instructor->searchCourses(); break; }
-			case 2: { instructor->printSchedule(); break; }
-			case 3: { instructor->printClasslist(); break; }
-			case 4: { instructor->printAll(); break; }
+			case 2: { instructor->printRosters(); break; }
+			case 3: { instructor->printAll(); break; }
 			case 0: {
 				do {
 					if (signedIn != 0 && signedIn != 1)
-						std::cout << "Please enter a valid input.\n\n";
+						std::cout << "Please enter a valid choice.\n\n";
 					std::cout << "You wish to sign out?\n";
 					std::cout << "1: Stay signed in\n";
 					std::cout << "0: Sign out\n";
@@ -81,7 +78,7 @@ void instructorHome(instructor_c* instructor) {
 				} while (signedIn != 0 && signedIn != 1);
 				break;
 			}
-			default: { std::cout << "Please enter a valid input.\n\n"; }
+			default: { std::cout << "Please enter a valid choice.\n\n"; }
 		}
 	}
 }
@@ -119,7 +116,7 @@ void adminHome(const char* dir, const int& MAXSTUDENT, const int& MAXINSTRUCTOR,
 			case 0: {
 				do {
 					if (signedIn != 0 && signedIn != 1)
-						std::cout << "Please enter a valid input.\n\n";
+						std::cout << "Please enter a valid choice.\n\n";
 					std::cout << "You wish to sign out?\n";
 					std::cout << "1: Stay signed in\n";
 					std::cout << "0: Sign out\n";
@@ -128,7 +125,7 @@ void adminHome(const char* dir, const int& MAXSTUDENT, const int& MAXINSTRUCTOR,
 				} while (signedIn != 0 && signedIn != 1);
 				break;
 			}
-			default: { std::cout << "Please enter a valid input.\n\n"; }
+			default: { std::cout << "Please enter a valid choice.\n\n"; }
 		}
 	}
 }
