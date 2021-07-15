4b84f76440ce49058d99052a23e879ae09237510
rpasquel <60162508+rpasquel@users.noreply.github.com>
2021-07-14 21:42:41 -0400

    Update userHome.cpp

--- userHome.cpp
+++ userHome.cpp
@@ -7,6 +7,9 @@
 /////////////////////////////////////////////////////
 void studentHome(const char* dir, student_c* student) {
 
+	std::ifstream inputStream;
+	inputStream.open("C:\\Users\\pasquelr\\Desktop\\LeopardWebAssignment\\The_Tests\\TestUserSignInOut\\Test_userHome_Student.txt");
+
 	int signedIn = 1;
 
 	while (signedIn) {
@@ -18,30 +21,33 @@ void studentHome(const char* dir, student_c* student) {
 		std::cout << "3: Display schedule\n";
 		std::cout << "4: Display all student info\n";
 		std::cout << "0: Sign out\n";
-		std::cin >> choice;
+		//std::cin >> choice;
+		inputStream >> choice;
 		//choice = 1; // * * * TEST * * *
 		std::cout << std::endl;
 
 		switch (choice) {
-			case 1: { student->searchCourses(dir); break; }
-			case 2: { student->addDropCourse(dir); break; }
-			case 3: { student->printSchedule(dir); break; }
-			case 4: { student->printAll(); break; }
-			case 0: {
-				do {
-					if (signedIn != 0 && signedIn != 1)
-						std::cout << "Please enter a valid choice.\n\n";
-					std::cout << "You wish to sign out?\n";
-					std::cout << "1: Stay signed in\n";
-					std::cout << "0: Sign out\n";
-					std::cin >> signedIn;
-					std::cout << std::endl;
-				} while (signedIn != 0 && signedIn != 1);
-				break;
-			}
-			default: { std::cout << "Please enter a valid choice.\n\n"; }
+		case 1: { student->searchCourses(dir); break; }
+		case 2: { student->addDropCourse(dir); break; }
+		case 3: { student->printSchedule(dir); break; }
+		case 4: { student->printAll(); break; }
+		case 0: {
+			do {
+				if (signedIn != 0 && signedIn != 1)
+					std::cout << "Please enter a valid choice.\n\n";
+				std::cout << "You wish to sign out?\n";
+				std::cout << "1: Stay signed in\n";
+				std::cout << "0: Sign out\n";
+				//std::cin >> signedIn;
+				inputStream >> signedIn;
+				std::cout << std::endl;
+			} while (signedIn != 0 && signedIn != 1);
+			break;
+		}
+		default: { std::cout << "Please enter a valid choice.\n\n"; }
 		}
 	}
+	inputStream.close();
 }
 
 
@@ -50,6 +56,9 @@ void studentHome(const char* dir, student_c* student) {
 // * * * * * * * * * * INSTRUCTOR * * * * * * * * * * //
 ////////////////////////////////////////////////////////
 void instructorHome(const char* dir, instructor_c* instructor) {
+
+	std::ifstream inputStream;
+	inputStream.open("C:\\Users\\pasquelr\\Desktop\\LeopardWebAssignment\\The_Tests\\TestUserSignInOut\\Test_userHome_Instructor.txt");
 
 	int signedIn = 1;
 
@@ -61,28 +70,31 @@ void instructorHome(const char* dir, instructor_c* instructor) {
 		std::cout << "2: Display Course Roster\n";
 		std::cout << "3: Display all instructor info\n";
 		std::cout << "0: Sign out\n";
-		std::cin >> choice;
+		//std::cin >> choice;
+		inputStream >> choice;
 		std::cout << std::endl;
 
 		switch (choice) {
-			case 1: { instructor->searchCourses(dir); break; }
-			case 2: { instructor->printRosters(dir); break; }
-			case 3: { instructor->printAll(); break; }
-			case 0: {
-				do {
-					if (signedIn != 0 && signedIn != 1)
-						std::cout << "Please enter a valid choice.\n\n";
-					std::cout << "You wish to sign out?\n";
-					std::cout << "1: Stay signed in\n";
-					std::cout << "0: Sign out\n";
-					std::cin >> signedIn;
-					std::cout << std::endl;
-				} while (signedIn != 0 && signedIn != 1);
-				break;
-			}
-			default: { std::cout << "Please enter a valid choice.\n\n"; }
+		case 1: { instructor->searchCourses(dir); break; }
+		case 2: { instructor->printRosters(dir); break; }
+		case 3: { instructor->printAll(); break; }
+		case 0: {
+			do {
+				if (signedIn != 0 && signedIn != 1)
+					std::cout << "Please enter a valid choice.\n\n";
+				std::cout << "You wish to sign out?\n";
+				std::cout << "1: Stay signed in\n";
+				std::cout << "0: Sign out\n";
+				//std::cin >> signedIn;
+				inputStream >> signedIn;
+				std::cout << std::endl;
+			} while (signedIn != 0 && signedIn != 1);
+			break;
+		}
+		default: { std::cout << "Please enter a valid choice.\n\n"; }
 		}
 	}
+	inputStream.close();
 }
 
 
@@ -91,7 +103,10 @@ void instructorHome(const char* dir, instructor_c* instructor) {
 // * * * * * * * * * * ADMIN * * * * * * * * * * //
 ///////////////////////////////////////////////////
 void adminHome(const char* dir, const int& MAXSTUDENT, const int& MAXINSTRUCTOR, const int& MAXADMIN, student_c* signedInStudent, instructor_c* signedInInstructor, admin_c* admin, int& numStudent, int& numInstructor, int& numAdmin) {
-	
+
+	std::ifstream inputStream;
+	inputStream.open("C:\\Users\\pasquelr\\Desktop\\LeopardWebAssignment\\The_Tests\\TestUserSignInOut\\Test_userHome_Admin.txt");
+
 	int signedIn = 1;
 
 	while (signedIn) {
@@ -106,30 +121,33 @@ void adminHome(const char* dir, const int& MAXSTUDENT, const int& MAXINSTRUCTOR,
 		std::cout << "6: Display courses\n";
 		std::cout << "7: Display all admin info\n";
 		std::cout << "0: Sign out\n";
-		std::cin >> choice;
+		//std::cin >> choice;
+		inputStream >> choice;
 		std::cout << std::endl;
 
 		switch (choice) {
-			case 1: { admin->addRemoveCourses(dir); break; }
-			case 2: { admin->addRemoveUsers(dir, MAXSTUDENT, MAXINSTRUCTOR, MAXADMIN, signedInStudent, signedInInstructor, admin, numStudent, numInstructor, numAdmin); break; }
-			case 3: { admin->addDropStudents(dir); break; }
-			case 4: { admin->searchCourses(dir); break; }
-			case 5: { admin->searchRosters(dir); break; }
-			case 6: { admin->printCourses(dir); break; }
-			case 7: { admin->printAll(); break; }
-			case 0: {
-				do {
-					if (signedIn != 0 && signedIn != 1)
-						std::cout << "Please enter a valid choice.\n\n";
-					std::cout << "You wish to sign out?\n";
-					std::cout << "1: Stay signed in\n";
-					std::cout << "0: Sign out\n";
-					std::cin >> signedIn;
-					std::cout << std::endl;
-				} while (signedIn != 0 && signedIn != 1);
-				break;
-			}
-			default: { std::cout << "Please enter a valid choice.\n\n"; }
+		case 1: { admin->addRemoveCourses(dir); break; }
+		case 2: { admin->addRemoveUsers(dir, MAXSTUDENT, MAXINSTRUCTOR, MAXADMIN, signedInStudent, signedInInstructor, admin, numStudent, numInstructor, numAdmin); break; }
+		case 3: { admin->addDropStudents(dir); break; }
+		case 4: { admin->searchCourses(dir); break; }
+		case 5: { admin->searchRosters(dir); break; }
+		case 6: { admin->printCourses(dir); break; }
+		case 7: { admin->printAll(); break; }
+		case 0: {
+			do {
+				if (signedIn != 0 && signedIn != 1)
+					std::cout << "Please enter a valid choice.\n\n";
+				std::cout << "You wish to sign out?\n";
+				std::cout << "1: Stay signed in\n";
+				std::cout << "0: Sign out\n";
+				//std::cin >> signedIn;
+				inputStream >> signedIn;
+				std::cout << std::endl;
+			} while (signedIn != 0 && signedIn != 1);
+			break;
+		}
+		default: { std::cout << "Please enter a valid choice.\n\n"; }
 		}
 	}
+	inputStream.close();
 }
